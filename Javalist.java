114d685cb98848d2610d03ff9a362d961108f146
llcain <llcain2012@gmail.com>
2023-07-29 23:08:06 -0500

    How to sort through array using 4-loop

--- Javalist.java
+++ Javalist.java
@@ -8,5 +8,22 @@ public class Javalist {
 
         System.out.println(Arrays.toString(array));
 
+        int[] numbers = new int[] {78, 34, 1, 3, 90, 34, 55, 20,  25};
+
+        // sorting logic
+
+//        for(int i = 0; i < numbers.length; i++) {
+//            for (int j = i + 1; j < numbers.length; j++) {
+//                if (numbers[i] > numbers[j]) {
+//                    int tmp = 0;
+//                    tmp = numbers[i];
+//                    numbers[i] = numbers[j];
+//                    numbers[j] = tmp;
+//                }
+//
+//            }
+//        }
+
+
     }
 }
