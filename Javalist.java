7a03c353873cae6224edf9b470b98d018e22f464
llcain <llcain2012@gmail.com>
2023-07-29 20:37:16 -0500

    print array to string using to string method

--- Javalist.java
+++ Javalist.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+
 public class Javalist {
 
     public static void main(String[] args) {
+        // defining an array of integer type
+       int[] array = new int[] {90, 10, 6, 17, 109};
 
-        System.out.println("laQuisha");
+        System.out.println(Arrays.toString(array));
 
     }
 }
